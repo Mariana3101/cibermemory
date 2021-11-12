c2a132bb254386b443e252442d3ce34fd946ad72
Mariana Duran <mariana3101@gmail.com>
2021-11-12 11:15:59 -0300

    se pasa el front de favoritos.php y mensajes.php

--- vistas/modulos/favoritos.php
+++ vistas/modulos/favoritos.php
@@ -1,142 +1,57 @@
 
 		<!-- Wrapper -->
 <?php include "vistas/templates/nav_buscar.php"; ?>
-					  
-					<section id="favoritos">
+	<style>
+      img{  object-fit: cover;
+          width: 75px;
+          height: 75px;
+          float: left;
+          margin-right:40px;
+          margin-left:40px
+          }
+    </style>
+				  
+				
 					
-						<div>
-							<!--tarjeta 1-->
-							<div class="">
-							
-							  	<div class="card-body">
-								
-									<h4 class="card-title">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-									<path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
-									</svg> Casa del abuelo</h4>
-
-								  	<div class="dropdown  d-md-flex justify-content-md-end">
-										<button class="btn dropdown-toggle" type="button"  data-bs-toggle="dropdown" aria-expanded="false">
-											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-												<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-											</svg>
-										</button>
-								
-										<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-										<li><a class="dropdown-item" href="#">Renderizar</a></li>
-										<li><a class="dropdown-item" href="#">Borrar</a></li>
-										<li><a class="dropdown-item" href="#">Agregar a Favoritos</a></li>
-										<li><a class="dropdown-item" href="#">Editar</a></li>
-										<li><a class="dropdown-item" href="#">Cambiar Imagen </a></li>
-										</ul>
-									</div>
-
-								
-							  	</div>
-							</div>
-							<hr> <!-- es la linea horizontal -->
-							<!--fin tarjeta 1-->
-
-							<!--tarjeta 2 -->
-							<div class="">
-							
-								<div class="card-body">
-								  
-								  	<h4 class="card-title">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-									  <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
-									</svg>  Outplays de Fornite</h4>
-  
-								   <!--menu desplegable-->
-									<div class="dropdown  d-md-flex justify-content-md-end">
-									  	<button class="btn dropdown-toggle" type="button"  data-bs-toggle="dropdown" aria-expanded="false">
-										  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-											  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-											</svg>
-									  	</button>
-								  
-										<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-											<li><a class="dropdown-item" href="#">Renderizar</a></li>
-											<li><a class="dropdown-item" href="#">Borrar</a></li>
-											<li><a class="dropdown-item" href="#">Agregar a Favoritos</a></li>
-											<li><a class="dropdown-item" href="editar.php">Editar</a></li>
-											<li><a class="dropdown-item" href="#">Cambiar Imagen </a></li>
-										</ul>
-								  	</div>
-								  <!--fin menu desplegable-->
-								  
-								</div>
-							</div>
-							<hr>
-							  <!--fin tarjeta 2 -->
-							  <!--tarjeta 3-->
-							<div class="">
-							
-								<div class="card-body">
-								  
-								  	<h4 class="card-title">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-									  <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
-									</svg> Comidas favoritas</h4> 
-  
-									<div class="dropdown  d-md-flex justify-content-md-end">
-										<button class="btn dropdown-toggle" type="button"  data-bs-toggle="dropdown" aria-expanded="false">
-											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-												<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-												</svg>
-										</button>
-									
-										<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-											<li><a class="dropdown-item" href="#">Renderizar</a></li>
-											<li><a class="dropdown-item" href="#">Borrar</a></li>
-											<li><a class="dropdown-item" href="#">Agregar a Favoritos</a></li>
-											<li><a class="dropdown-item" href="editar.php">Editar</a></li>
-											<li><a class="dropdown-item" href="#">Cambiar Imagen </a></li>
-										</ul>
-								  	</div>
-  
-								  
-								</div>
-							</div>
-							  <!--fin tarjeta 3-->
-							  <hr>
-							  <!--tarjeta 4-->
-							<div class="">
-							
-								<div class="card-body">
-								  
-								  	<h4 class="card-title">	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-									  <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
-									</svg> Risas más fuertes</h4>
-  
-									<div class="dropdown  d-md-flex justify-content-md-end">
-									  	<button class="btn dropdown-toggle" type="button"  data-bs-toggle="dropdown" aria-expanded="false">
-										  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-											  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-											</svg>
-											<i class="bi bi-folder"></i>
-									  	</button>
-								  
-										<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-											<li><a class="dropdown-item" href="#">Renderizar</a></li>
-											<li><a class="dropdown-item" href="#">Borrar</a></li>
-											<li><a class="dropdown-item" href="#">Agregar a Favoritos</a></li>
-											<li><a class="dropdown-item" href="editar.php">Editar</a></li>
-											<li><a class="dropdown-item" href="#">Cambiar Imagen </a></li>
-										</ul>
-								  	</div>
-  
-								  
-								</div>
-							</div>
-							<hr>
-							<!--fin tarjeta 4-->
-						</div>
-
-						
-						
-					</section>
-					<!--Fin section favoritos-->
-				</div>
-			</section>
-		</div>		
-			
+	<section id="Favoritos" class="container">
+        <section id="intro" class="wrapper style1 fullscreen fade-up">
+            <div class="inner row">
+               <!-- <span class="card" style="width: 18rem;">-->
+                  	<div class="wrap col-12">
+                    
+                    	<img type = "img/png" src="https://e1.pngegg.com/pngimages/359/574/png-clipart-label-folders-purple-folder-icon.png"></iframe>
+                    	<br>
+                    	<h3 class="card-title">Plays de Fortnite</h3>
+                    	<br>
+                    	<img type = "img/png" src="https://e1.pngegg.com/pngimages/359/574/png-clipart-label-folders-purple-folder-icon.png"></iframe>
+                    	<br>
+                    	<h3 class="card-title">Explote de risa</h3>
+                    	<br>
+                    	<img type = "img/png" src="https://e1.pngegg.com/pngimages/359/574/png-clipart-label-folders-purple-folder-icon.png"></iframe>
+                    	<br>
+                    	<h3 class="card-title">Mejores salidas</h3>
+                    	<br>
+                    	<img type = "img/png" src="https://e1.pngegg.com/pngimages/359/574/png-clipart-label-folders-purple-folder-icon.png"></iframe>
+                    	<br>
+                    	<h3 class="card-title">Momentos Extremos</h3>
+                    	<br>
+                    	<img type = "img/png" src="https://e1.pngegg.com/pngimages/359/574/png-clipart-label-folders-purple-folder-icon.png"></iframe>
+                    	<br>
+                    	<h3 class="card-title">Charlas Copadas</h3>
+                    	<br>
+                 	</div>           
+            	</div>
+            </div>
+          
+                                
+                                
+                               
+                      
+        </section>
+                        
+                        
+     </section>
+                  
+	
 
 
--- vistas/modulos/mensajes.php
+++ vistas/modulos/mensajes.php
@@ -1,20 +1,70 @@
 
 
 	<!-- Wrapper -->
-    <div id="wrapper">
-			<section id="intro" class="wrapper style1 fullscreen fade-up">
-				<div class="inner">
-				<section style="background-color: aquamarine;">
-                    <h1>hola </h1>
-                </section>
-                <section style="background-color: yellow;">
-                    <h1>a </h1>
-                </section>
-                <section style="background-color: red;">
-                    <h1>todos</h1>
-                </section>
-					<!--Fin select-->
-				</div>
-			</section>
-		</div>	
+    <style>
+        img{  object-fit: cover;
+              width: 75px;
+              height: 75px;
+              border-radius: 38px;
+              float: left;
+              margin-right:40px;
+              margin-left:40px
+              }
+        </style>
+
+    <div id="wrapper container">
+		<section id="intro" class="wrapper style1 fullscreen fade-up">
+            <div class="inner">
+                <a href="#" class="btn btn-primary"> Volver</a>
+            </div>
+                <div class="inner row">
+                  <!--  <span class="card" style="width: 18rem;"> -->
+                        
+                        <div class="wrap col-12">
+                            <div class="box " >
+                                <img src="https://photos.headshotcrew.com/styles/large/s3/photos/08/53/23/alexiscpeterhurley.jpg?itok=ujCkOepQ"></iframe>
+                                <h3 class="card-title">Carlitos</h3>
+                                <p class="card-text">Hola, todo bien?</p>
+                            </div>
+                            <div class="box ">
+                                <img src="https://erenovable.com//wp-content/uploads/2016/08/cuales-son-las-flores-de-primavera.jpg"></iframe>
+                                <h3 class="card-title">Mama</h3>
+                                <p class="card-text">dale, nos vemos </p>
+                            </div>
+                            <div class="box ">
+                                <img src="https://pbs.twimg.com/profile_images/445737210877607937/5o8GWWx9.jpeg"></iframe>
+                                <h3 class="card-title">Josefina</h3>
+                                <p class="card-text">jajaja</p>
+                            </div>
+                            <div class="box ">
+                                <img src="https://pbs.twimg.com/media/ESjZwywXYAAtisO.jpg"></iframe>
+                                <h3 class="card-title">Guille</h3>
+                                <p class="card-text">hola, estas? </p>
+                            </div>
+                            <div class="box">   
+                                <img src="https://t2.ea.ltmcdn.com/es/images/3/0/6/img_clases_de_caballos_20603_600.jpg"></iframe>
+                                <h3 class="card-title">Papa</h3>
+                                <p class="card-text">muy bueno eh? </p>
+                            </div>
+                            <div class="box">
+                                <img src="https://i.pinimg.com/236x/63/c7/ca/63c7cad26671754e0db3ed927d0d9894--style-fashion.jpg"></iframe>
+                                <h3 class="card-title">Sofia</h3>
+                                <p class="card-text">que carajos! </p>
+                            </div>
+    
+                        </div>
+                       
+                      
+                                  
+                                    
+                </div>
+            
+                        
+                            
+                            
+                           
+          
+>
+		</section>
+	</div>	
 
