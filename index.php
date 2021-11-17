b052fc1464524d5c4823f8a9f0a96b9374c7a185
Mariana Duran <mariana3101@gmail.com>
2021-11-17 11:25:56 -0300

    .

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once "Controladores/adminC.php";
 require_once "Modelos/adminM.php";
 require_once "Controladores/recuerdoC.php";
 require_once "Modelos/recuerdoM.php";
+#require_once "Controladores/validacionesC.php";
 
 $rutas = new RutasControlador();
 $rutas->Plantilla();
